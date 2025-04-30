2dcbf87b8683c351bd69995ab54898b88b6db594
jbwilson1120@gmail.com <jbwilson1120@gmail.com>
2025-04-29 21:04:07 -0400

    error fixes

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ echo 'Hello World!';
 echo '</br>';
 echo '</br>Loading config...';
 require_once 'config.php';      //Main settings
-echo '</br>' + $NUMBER_OF_DOORS;
+echo '</br>' . $NUMBER_OF_DOORS;
 echo '</br>Loading pinconfig...';
 require_once 'pinconfig.php';   //Pin mapping
 echo '</br>Loading class GarageDoor...';
@@ -15,6 +15,6 @@ echo 'initializing garage doors...';
 $door1 = new GarageDoor(1, 'Left Door', $DOOR1_OPEN_SENSOR, $DOOR1_CLOSED_SENSOR, $DOOR1_BUTTON);
 $door2 = new GarageDoor(2, 'Right Door', $DOOR2_OPEN_SENSOR, $DOOR2_CLOSED_SENSOR, $DOOR2_BUTTON);
 echo '</br>';
-echo $door1->name + ': [STATUS TBD]';
+echo $door1->name . ': [STATUS TBD]';
 echo '</br>';
-echo $door2->name + ': [STATUS TBD]';
+echo $door2->name . ': [STATUS TBD]';

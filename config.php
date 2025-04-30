49f6f2fe66a4178822e21001d5499c157334b1d6
jbwilson1120@gmail.com <jbwilson1120@gmail.com>
2025-04-29 21:02:57 -0400

    fixed text qualifiers in config

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php
 // Config settings
-$PASSWORD = "202501985";
-$BG_COLOR_UNKNOWN = "orange";
-$BG_COLOR_OPEN = "red";
-$BG_COLOR_CLOSED = "green";
-$IMAGE_UNKNOWN = "GarageQuestion.gif";
-$IMAGE_OPEN  = "GarageRed.gif";
-$IMAGE_CLOSED = "GarageGreen.gif";
+$PASSWORD = '202501985';
+$BG_COLOR_UNKNOWN = 'orange';
+$BG_COLOR_OPEN = 'red';
+$BG_COLOR_CLOSED = 'green';
+$IMAGE_UNKNOWN = 'GarageQuestion.gif';
+$IMAGE_OPEN  = 'GarageRed.gif';
+$IMAGE_CLOSED = 'GarageGreen.gif';
 $NUMBER_OF_DOORS = 2;
-$DOOR_1_NAME = "Left Door";
-$DOOR_2_NAME = "Right Door";
+$DOOR_1_NAME = 'Left Door';
+$DOOR_2_NAME = 'Right Door';
 $SENSORS_PER_DOOR = 2;
-$APIKEY = "u^Vfj&FEI&fUD9w^";
-$ADMIN = "admin332";
-$ADMIN_PASS = "north$h0r3";
+$APIKEY = 'u^Vfj&FEI&fUD9w^';
+$ADMIN = 'admin332';
+$ADMIN_PASS = 'north$h0r3';

2a28473c1ccac47258dfd79ba19e26e6d0575522
jbwilson1120@gmail.com <jbwilson1120@gmail.com>
2025-04-29 20:57:21 -0400

    status messages

--- config.php
+++ config.php
@@ -1,13 +1,10 @@
 <?php
 // Config settings
-$ENABLE_PASSWORD  = "YES";
 $PASSWORD = "202501985";
-$ENABLE_SIRI = "NO";
-$SIRI_PASSWORD = "12345678";
-$BG_COLOR_QUESTION = "orange";
+$BG_COLOR_UNKNOWN = "orange";
 $BG_COLOR_OPEN = "red";
 $BG_COLOR_CLOSED = "green";
-$IMAGE_QUESTION = "GarageQuestion.gif";
+$IMAGE_UNKNOWN = "GarageQuestion.gif";
 $IMAGE_OPEN  = "GarageRed.gif";
 $IMAGE_CLOSED = "GarageGreen.gif";
 $NUMBER_OF_DOORS = 2;
@@ -17,4 +14,3 @@ $SENSORS_PER_DOOR = 2;
 $APIKEY = "u^Vfj&FEI&fUD9w^";
 $ADMIN = "admin332";
 $ADMIN_PASS = "north$h0r3";
-?>
--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
 echo 'Hello World!';
-echo 'Loading Garage doors...';
-
+echo '</br>';
+echo '</br>Loading config...';
 require_once 'config.php';      //Main settings
-echo '</br>'+$NUMBER_OF_DOORS;
+echo '</br>' + $NUMBER_OF_DOORS;
+echo '</br>Loading pinconfig...';
 require_once 'pinconfig.php';   //Pin mapping
+echo '</br>Loading class GarageDoor...';
 require_once 'GarageDoor.php';  //Main Garage Door class
 
 // Initialize the doors
+echo 'initializing garage doors...';
 $door1 = new GarageDoor(1, 'Left Door', $DOOR1_OPEN_SENSOR, $DOOR1_CLOSED_SENSOR, $DOOR1_BUTTON);
 $door2 = new GarageDoor(2, 'Right Door', $DOOR2_OPEN_SENSOR, $DOOR2_CLOSED_SENSOR, $DOOR2_BUTTON);
 echo '</br>';

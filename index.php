e65821ffe0608b2df916bae43de38b3278bfc792
jbwilson1120@gmail.com <jbwilson1120@gmail.com>
2025-07-11 23:03:48 -0400

    tags fix

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require_once dirname(__FILE__) . '/piPHP_GPIO/Pin/PinInterface.php';
 
 // Initialize the doors
 require_once dirname(__FILE__) . '/InitDoors.php';
-$htmlbody = $u->HTMLTemplate("doorstatus.txt", "");
+
 $tags['doorstatussize'] = '50';
 
 $tags['D1Name'] = $door1->name;
@@ -19,4 +19,6 @@ $tags['door1status'] = $door1->GetStateImage();
 $tags['D2Name'] = $door2->name;
 $tags['door2visable'] = 'block';
 $tags['door2status'] = $door2->GetStateImage();
+
+$htmlbody = $u->HTMLTemplate("doorstatus.txt", $tags);
 echo($htmlbody);

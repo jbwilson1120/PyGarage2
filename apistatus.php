2e94f7072b0f6360f9eb678b1fdf9cb685acad2e
jbwilson1120@gmail.com <jbwilson1120@gmail.com>
2025-07-12 16:44:20 -0400

    return all doors if no target provided

--- apistatus.php
+++ apistatus.php
@@ -10,21 +10,23 @@
 // Initialize the doors
 require_once dirname(__FILE__) . '/InitDoors.php';
 
+// Initialize the response array
+$response = [];
+
 $door = $_GET["door"];
 switch ($door) {
     case 'door1':
-        $targetdoor = $door1;
+        $response['doors'] = ['door' => $door1->name, 'status' => $door1->GetState()];
     case 'door2':
-        $targetdoor = $door2;
+        $response['doors'] = ['door' => $door2->name, 'status' => $door2->GetState()];
     default:
-
+        $response['doors'] = [['door' => $door1->name, 'status' => $door1->GetState()], ['door' => $door2->name, 'status' => $door2->GetState()]];
 }
 
 
 // Set the content type to JSON
 header('Content-Type: application/json');
-// Initialize the response array
-$response = [];
+
 // Check the state of the target door and add it to the response
 $response['doors'] = ['status' => $targetdoor->GetState()];
 // return the response as JSON
